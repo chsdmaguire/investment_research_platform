869a0cb167dd60f85ee3bfd21bedaa6e3f42fc70
chrisflibyrd <83556303+chrisflibyrd@users.noreply.github.com>
2021-12-28 14:42:11 -0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,9 +8,10 @@
   analytics.page();
   }}();
 </script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2719099872585013"
+     crossorigin="anonymous"></script>  
     </head>
-    <!-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2719099872585013"
-     crossorigin="anonymous"></script>   -->
+
 
 
     <Nuxt />
