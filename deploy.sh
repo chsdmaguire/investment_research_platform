311b6cb0221c2e4b11cbbabdcc180e8982558f4f
Jonny Lin <jonnylin@ubermedia.com>
2022-02-02 11:30:25 -0800

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-git pull origin master || echo "Authenticate with GitHub SSH before running this script"
+git pull origin master || echo "Authenticate with GitHub SSH before running this script" && exit
 echo "=============================="
 echo "Pulled latest code from master"
 echo "=============================="
 
-npm i || echo "Failed to update dependencies"
+npm i || echo "Failed to update dependencies" && exit
 echo "============"
 echo "Updated deps"
 echo "============"
 
-nuxt build || echo "Failed nuxt build"
+npm run build || echo "Failed nuxt build" && exit
 echo "============================="
 echo "Built latest code from master"
 echo "============================="
 
-pm2 start || echo "pm2 failed to launch daemon"
+pm2 start || echo "pm2 failed to launch daemon, check ecosystem.config.js" && exit
 echo "===="
 echo "Done"
 echo "===="
