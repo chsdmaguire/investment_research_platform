3fd896348a0c2ba6bd52fc3023f77da3561d7577
Jonny Lin <jonnylin@ubermedia.com>
2022-02-02 12:52:25 -0800

    Test CD 1

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 git pull origin master || (echo "Authenticate with GitHub SSH before running this script" && exit)
-echo "=============================="
+echo -e "\n=============================="
 echo "Pulled latest code from master"
 echo "=============================="
 
 npm i || (echo "Failed to update dependencies" && exit)
-echo "============"
+echo -e "\n============"
 echo "Updated deps"
 echo "============"
 
 npm run build || (echo "Failed nuxt build" && exit)
-echo "============================="
+echo -e "\n============================="
 echo "Built latest code from master"
 echo "============================="
 
 pm2 start || (echo "pm2 failed to launch daemon, check ecosystem.config.js" && exit)
-echo "===="
+echo -e "\n===="
 echo "Done"
 echo "===="
