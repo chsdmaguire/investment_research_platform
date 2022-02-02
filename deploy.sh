3110040c20f3ed079c7745ec653d4b61a10e20cf
Jonny Lin <jonnylin@ubermedia.com>
2022-02-02 14:28:28 -0800

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-git pull origin master || exit
+git pull origin master || exit 1
 echo -e "\n=============================="
 echo "Pulled latest code from master"
 echo "=============================="
 
-npm i || exit
+npm i || exit 1
 echo -e "\n============"
 echo "Updated deps"
 echo "============"
 
-npm run build || exit
+npm run build || exit 1
 echo -e "\n============================="
 echo "Built latest code from master"
 echo "============================="
 
-pm2 start || exit
+pm2 reload flibyrd-nuxt || exit 1
 echo -e "\n===="
 echo "Done"
 echo "===="
+exit 0
