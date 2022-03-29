f7934b02dfb9847f5ccec8b9f054d65266d32a02
chrisflibyrd <chris@flibyrd.com>
2022-03-29 08:10:22 -0700

    started basic info section

--- components/wm/PersonalInfo.vue
+++ components/wm/PersonalInfo.vue
@@ -1,99 +1,343 @@
 <template>
-  <v-container
-    class="mx-auto my-16"
-  >
-    <v-card-title class="text-h6 font-weight-regular justify-space-between">
-      <span>{{ currentTitle }}</span>
-    </v-card-title>
+  <v-container>
+     <v-stepper v-model="e1" non-linear>
+            <v-stepper-header>
+            <v-stepper-step step="1" editable>
+                Intro
+            </v-stepper-step>
 
-    <v-window v-model="step" show-arrows>
-            <template v-slot:prev="{ on, attrs }">
+            <v-divider></v-divider>
+
+            <v-stepper-step editable step="2">
+                Age
+            </v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step editable step="3">
+                Status
+            </v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step editable step="4">
+                Dependents
+            </v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step editable step="5">
+                Residence
+            </v-stepper-step>
+            <v-divider></v-divider>
+            <v-stepper-step editable step="6">
+                Goals
+            </v-stepper-step>
+            </v-stepper-header>
+
+            <v-stepper-items>
+              <v-stepper-content step="1">
+                <v-card
+                color="#121212"
+                height="200px"
+                >
+                <v-row justify="center" align="center" class="mt-5">
+                  <h3 class="question">
+                        First, let's start with some basic info so we can get to know you better.
+                    </h3>
+                </v-row>
+                <!-- GET FULL NAME AND TITLE -->
+                </v-card>
                 <v-btn
-                    color="success"
-                    v-bind="attrs"
-                    v-on="on"
-                >Previous</v-btn>
-            </template>
-            <template v-slot:next="{ on, attrs }">
-                <v-btn
-                    color="info"
-                    v-bind="attrs"
-                    v-on="on"
-                >Next</v-btn>
-            </template>
-<!-- AGE -->
-      <v-window-item :value="1">
-          <v-row class="my-16">
-              <v-col cols="8">
-                <v-slider v-model="age"
-                :thumb-size="50"
-                thumb-label="always"
-                min="1" max="120" >
-                    <template v-slot:prepend>
-                    <v-icon
-                        color="indigo"
-                        @click="decrement"
-                    >
-                        mdi-minus
-                    </v-icon>
+                color="primary"
+                @click="moveUp"
+                >
+                Continue
+                </v-btn>
+
+                <v-btn text @click="moveDown">
+                Back
+                </v-btn>
+              </v-stepper-content>
+            <v-stepper-content step="2">
+                <v-card
+                color="#121212"
+                height="200px"
+                >
+                <v-row justify="center" align="center" class="mt-5">
+                    <h3 class="question">
+                        When is your birthday?
+                    </h3>
+                </v-row>
+                <v-row justify="center" align="center">
+                  <v-col cols="6">
+                    <v-menu
+                    ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                        v-model="date"
+                        label="Birthday"
+                        readonly
+                        v-on="on"
+                      ></v-text-field>
                     </template>
+                    <v-row justify="center" align="center">
+                      <v-col cols="6">
+                    <v-date-picker
+                      ref="picker"
+                      full-width
+                      v-model="date"
+                      :day-format="date => new Date(date).getDate()"
+                      :max="new Date().toISOString().substr(0, 10)"
+                      :picker-date="pickerDate"
+                      min="1950-01-01"
+                      @change="save"
+                    ></v-date-picker>
+                      </v-col>
+                    </v-row>
+                  </v-menu>
+                  </v-col>
+                  
+                </v-row>
+                </v-card>
 
-                    <template v-slot:append>
-                    <v-icon
-                        color="indigo"
-                        @click="increment"
-                    >
-                        mdi-plus
-                    </v-icon>
-                    </template>         
-                </v-slider>
-            </v-col>
-          </v-row>
+                <v-btn
+                color="primary"
+                @click="moveUp"
+                >
+                Continue
+                </v-btn>
 
-      </v-window-item>
+                <v-btn text @click="moveDown">
+                Back
+                </v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="3">
+              <v-card
+                color="#121212"
+                height="600px"
+                >
+                <template v-if="!status">
+              <v-row justify="center" align="center" class="mt-5">
+                What is your marital status?
+              </v-row>
+              <v-row justify="center" align="center">
+                
+                <v-col cols="6">
+                  <v-row v-for="i, idx in relationships" :key="idx" 
+                  justify="center" align="center" v-model="maritalStatus">
+                    <v-btn block depressed @click="updateStatus(i)">{{ i }}</v-btn>
+                  </v-row>
+                </v-col>
+              </v-row>
+               </template>
+              <template v-if="status == 'engaged' ">
+                <v-row justify="center" align="center">
+                  <h3>When do you plan on getting married?</h3>
+                </v-row>
+                <v-row justify="center" align="center">
+                  <v-date-picker
+                      full-width
+                      v-model="statusChange"
+                      :day-format="date => new Date(date).getDate()"
+                      :picker-date="pickerDate2"
+                      :min="new Date().toISOString().substr(0, 10)"
+                      @change="saveStatus"
+                    ></v-date-picker>
+                </v-row>
+              </template>
+              <template v-if="status == 'divorced' ">
+                <v-row justify="center" align="center">
+                  <h3>When do get divorced?</h3>
+                </v-row>
+                <v-row justify="center" align="center">
+                  <v-date-picker
+                      full-width
+                      v-model="statusChange"
+                      :day-format="date => new Date(date).getDate()"
+                      :picker-date="pickerDate2"
+                      @change="saveStatus"
+                    ></v-date-picker>
+                </v-row>
+              </template>
+               <template v-if="status == 'widowed' ">
+                <v-row justify="center" align="center">
+                  <h3>When were you widowed?</h3>
+                </v-row>
+                <v-row justify="center" align="center">
+                  <v-date-picker
+                      full-width
+                      v-model="statusChange"
+                      :day-format="date => new Date(date).getDate()"
+                      :picker-date="pickerDate2"
+                      :min="new Date().toISOString().substr(0, 10)"
+                      @change="saveStatus"
+                    ></v-date-picker>
+                </v-row>
+              </template>
+              </v-card>
+              <v-btn
+                color="primary"
+                @click="moveUp"
+                >
+                Continue
+                </v-btn>
 
+                <v-btn text @click="moveDown">
+                Back
+                </v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="4">
+              <v-card
+                color="#121212"
+                height="400px"
+                >
+                <v-row justify="center" align="center" class="mt-5">
+                  <h3 class="question">
+                    Do you have any dependents?
+                  </h3>
+                </v-row>
+                <v-row justify="center" align="center" class="mt-5">
+                  <v-col cols="3">
+                    <v-card class="mx-auto rounded-circle" 
+                     width="170" height="170" color="indigo">
+                     <v-row justify="center" align="center">
+                       <h3 class="mt-16">Yes</h3>
+                     </v-row>
+                     </v-card>
+                  </v-col>
+                  <v-col cols="3">
+                    <v-card class="mx-auto rounded-circle" 
+                     width="170" height="170" color="indigo">
+                     <v-row justify="center" align="center">
+                       <h3 class="mt-16">No</h3>
+                     </v-row>
+                     </v-card>
+                     <!-- need to get each kids age & if they intend to 
+                     help pay for college if so how what % -->
+                  </v-col>
+                </v-row>
+              </v-card>
+              <v-btn
+                color="primary"
+                @click="moveUp"
+                >
+                Continue
+                </v-btn>
 
-      <v-window-item :value="2">
-      </v-window-item>
+                <v-btn text @click="moveDown">
+                Back
+                </v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="5">
+              <v-card
+                color="#121212"
+                height="400px"
+                >
+                <v-row justify="center" align="center">
 
-      <v-window-item :value="3">
-      </v-window-item>
-    </v-window>
+                </v-row>
+              </v-card>
+              <v-btn
+                color="primary"
+                @click="moveUp"
+                >
+                Continue
+                </v-btn>
 
-    <v-divider></v-divider>
+                <v-btn text @click="moveDown">
+                Back
+                </v-btn>
+            </v-stepper-content>
+            <v-stepper-content step="6">
+              <v-card
+                color="#121212"
+                height="400px"
+                >
+                <v-row justify="center" align="center">
 
+                </v-row>
+              </v-card>
+              <v-btn
+                color="primary"
+                @click="moveUp"
+                >
+                Continue
+                </v-btn>
+
+                <v-btn text @click="moveDown">
+                Back
+                </v-btn>
+            </v-stepper-content>
+            </v-stepper-items>
+        </v-stepper>
   </v-container>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            step: 1,
-            age: 30
-        }
+  data() {
+    return {
+      e1: 1,
+      date: null,
+      menu: false,
+      pickerDate: '1995-1-1',
+      pickerDate2: '',
+      relationships: ['single', 'engaged', 'married', 'divorced', 'widowed'],
+      maritalStatus: null,
+      status: '',
+      statusChange: null,
+      statusChangeDate: null
+    }
+  },
+  watch: {
+    menu (val) {
+      val && setTimeout(() => (
+        this.$refs.picker.activePicker = 'YEAR',
+        this.pickerDate = null
+      ))
     },
-    computed: {
-      currentTitle () {
-        switch (this.step) {
-          case 1: return 'What is your current age?'
-          case 2: return 'Do you own or rent?'
-          case 3: return 'What is your marital status?'
-          case 3: return 'What is your marital status?'
-        }
-      },
+  },
+  methods: {
+        moveUp() {
+            if(this.e1 < 6) {
+                this.e1 += 1
+            } else {
+                this.e1 = 6
+            }
+        },
+        moveDown() {
+            if(this.e1 > 1) {
+                this.e1 -= 1
+            }
+        },
+        save (date) {
+      this.$refs.menu.save(date)
+      this.pickerDate = date;
     },
-      methods: {
-          decrement() {
-              this.age --
-          },
-          increment() {
-              this.age ++
-          }
-      }
+    saveStatus(date) {
+      this.pickerDate2 = date;
+      console.log(this.statusChange)
+    },
+    updateStatus(val) {
+        this.status = val
+      console.log(val)
+    }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.v-stepper {
+    background-color: #121212 !important;
+}
+.v-stepper-content {
+    background-color: #121212 !important;
+}
 
+.v-stepper-header {
+    background-color: #121212 !important;
+}
 </style>
---
+++ pages/wealthmanagement.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+      <template v-if="!started">
+      <v-row justify="center" align="center" class="mt-5">
+            <div class="intro">
+                <h1>Welcome to Flibyrd's Automated Wealth Management Platform</h1>
+            </div>
+        </v-row>
+        <v-row align="center" justify="center" class="mt-8">
+            <div class="intro">
+                <h3>We understand financial planning is difficulty and costly. Our goal is to make this chore as fast and simple as possible for you.
+                    <span> <v-btn class="ml-2" color="#009688" @click="started = true">Yes, Please!</v-btn> </span>
+                </h3>
+            </div>
+        </v-row>
+      </template>
+      <template v-if="started">
+          <PersonalInfo />
+          <!-- 2. work/income (also habe to ask about spouse, 
+          show expected tax rate at end based on residency as well to ask if correct)
+        3. house, ask if rent or own. ask if more than one property & get mortgage details
+        4. Assets
+        5. other liabilities
+        6. risk tolerance/experience investing -->
+      </template>
+  </v-container>
+</template>
+
+<script>
+import PersonalInfo from '~/components/wm/PersonalInfo'
+export default {
+    components: {PersonalInfo},
+    data() {
+        return {
+            started: false,
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+.intro {
+    animation: fadeInAnimation linear 2s;
+}
+@keyframes fadeInAnimation {
+            0% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+</style>
