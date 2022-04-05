287819a1e42cdbcb9fee8314d2945d6b8d4b7a8a
chrisflibyrd <83556303+chrisflibyrd@users.noreply.github.com>
2022-04-05 13:45:39 -0700

    added IRAs & CDs

--- pages/newform.vue
+++ pages/newform.vue
@@ -182,56 +182,37 @@
           </v-col>
         </v-row>
     </v-carousel-item>
-     <v-carousel-item>
+    <v-carousel-item>
       <v-sheet>
         <v-row justify="center" align="center" class="mt-12">
           <h2>Do you own any of of the following types of IRAs?</h2>
         </v-row>
-        <v-row justify="center" align="center" class="mt-4">
-<v-card
-    class="mx-auto"
-    max-width="500"
-  >
-    <v-list shaped>
-      <v-list-item-group
-        v-model="iraModel"
-        multiple
-      >
-        <template v-for="(item, i) in listOfIras">
-          <v-list-item
-            :key="`item-${i}`"
-            :value="item"
-            active-class="deep-purple--text text--accent-4"
-          >
-            <template v-slot:default="{ active }">
-              <v-list-item-content>
-                <v-list-item-title v-text="item"></v-list-item-title>
-              </v-list-item-content>
-
-              <v-list-item-action>
-                <v-checkbox
-                  :input-value="active"
-                  color="deep-purple accent-4"
-                ></v-checkbox>
-              </v-list-item-action>
+        <v-row justify="center" align="center" class="mt-5" 
+        v-for="index in listOfIras.length / 4" :key="index">
+        <v-col md="3" sm="6" xs="12" v-for="(item, idx) in listOfIras.slice((index -1) *4, (index -1)*4 + 4)" :key="idx">
+          <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card :elevation="hover ? 12 : 4" @click="updateIraModel(item)" 
+              :color="iraModel.includes(item) ? 'deep-purple accent-4' : '#ffffff' ">
+              <v-row align="center" justify="center">
+                <v-card-title v-text="item"></v-card-title>
+              </v-row>            
+              </v-card>
             </template>
-          </v-list-item>
-        </template>
-      </v-list-item-group>
-    </v-list>
-  </v-card>
+          </v-hover>
+        </v-col>
         </v-row>
-        <v-row justify="center" align="center" class="mt-10">
+        <v-row justify="center" align="center" class="mt-1">
           <v-col cols="1">
             <v-btn @click="goBack">Back</v-btn>
           </v-col>
           <v-col cols="1">
             <v-btn @click="updateIraList">Continue</v-btn>
           </v-col>
-        </v-row>
+        </v-row>            
       </v-sheet>
     </v-carousel-item>
-     <v-carousel-item>
+    <v-carousel-item>
       <v-sheet>
         <v-row justify="center" align="center" class="mt-12">
           <h2>Please enter current market value for each IRA</h2>
@@ -259,8 +240,108 @@
             <v-btn @click="goBack">Back</v-btn>
           </v-col>
           <v-col cols="1">
-            <v-btn @click="upOne">Continue</v-btn>
+            <v-btn @click="updateIras">Continue</v-btn>
           </v-col>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item>
+      <v-sheet>
+        <v-row justify="center" align="center" class="mt-12">
+          <h2>Do you own any of of the following types of CDs?</h2>
+        </v-row>
+        <v-row justify="center" align="center" class="mt-5" 
+        v-for="index in listOfCds.length / 4" :key="index">
+        <v-col md="3" sm="6" xs="12" v-for="(item, idx) in listOfCds.slice((index -1) *4, (index -1)*4 + 4)" :key="idx">
+          <v-hover>
+            <template v-slot:default="{ hover }">
+              <v-card :elevation="hover ? 12 : 4" @click="updateCdModel(item)" 
+              :color="cdModel.includes(item) ? 'deep-purple accent-4' : '#ffffff' ">
+              <v-row align="center" justify="center">
+                <v-card-title v-text="item"></v-card-title>
+              </v-row>            
+              </v-card>
+            </template>
+          </v-hover>
+        </v-col>
+        </v-row>
+        <v-row justify="center" align="center" class="mt-1">
+          <v-col cols="1">
+            <v-btn @click="goBack">Back</v-btn>
+          </v-col>
+          <v-col cols="1">
+            <v-btn @click="updateCdList">Continue</v-btn>
+          </v-col>
+        </v-row>            
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item>
+      <v-sheet>
+        <v-row justify="center" align="center" class="mt-12">
+          <h2>Please enter info for each CD</h2>
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-col cols="12">
+            <v-row align="center" justify="center" v-for="(n, idx) in cds" :key="idx">
+                <v-col cols="3">
+                  <v-text-field hide-details label="Type" v-model="n.type"></v-text-field>
+                </v-col>
+                <v-col cols="2">
+                    <v-text-field
+                      v-model="n.deposit_amount"
+                      hide-details
+                      single-line
+                      type="number"
+                      label="Deposit Amount"
+                    />
+                </v-col>
+                <v-col cols="2">
+                  <v-text-field v-model="n.deposit_date" label="Deposit Date"
+                  hide-details single-line type="date"></v-text-field>
+                </v-col>
+                <v-col cols="2">
+                  <v-autocomplete v-model="n.term" :items="cdTerms" label="Term"
+                  rounded filled dense class="mb-n10"></v-autocomplete> 
+                </v-col>
+                <v-col cols="1">
+                  <v-text-field hide-details single-line type="number" label="APY" append-icon="mdi-percent"
+                  v-model="n.apy" />
+                </v-col>
+              </v-row>
+          </v-col>     
+        </v-row>
+        <v-row justify="center" align="center" class="mt-10">
+          <v-col cols="1">
+            <v-btn @click="goBack">Back</v-btn>
+          </v-col>
+          <v-col cols="1">
+            <v-btn @click="updateCds">Continue</v-btn>
+          </v-col>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item>
+      <v-sheet>
+        <v-row justify="center" align="center" class="mt-12">
+          <h2>Do you have any of the following employer-sponsored retirement plans?</h2>
+        </v-row>
+        <v-row justify="center" align="center">
+          <v-col cols="3">
+            <v-card class="mx-auto rounded-circle" 
+                width="170" height="170" color="indigo" @click.stop="upOne">
+                <v-row justify="center" align="center">
+                <h3 class="mt-16">Yes</h3>
+                </v-row>
+            </v-card>
+            </v-col>
+            <v-col cols="3">
+            <v-card class="mx-auto rounded-circle" @click.stop="skipTwo"
+                width="170" height="170" color="indigo">
+                <v-row justify="center" align="center">
+                <h3 class="mt-16">No</h3>
+                </v-row>
+            </v-card>
+            </v-col>
         </v-row>
       </v-sheet>
     </v-carousel-item>
@@ -280,8 +361,8 @@ export default {
       checkings: 0,
       savings: 0,
       iras: [],
-      cds: 0,
-      mmfs: 0,
+      cds: [],
+      mmfs: [],
       brokerages: [],
       compRetPlans: [],
       collegePlans: [],
@@ -296,9 +377,18 @@ export default {
       listOfIras: ['Roth IRA', 'Traditional IRA', 'SIMPLE IRA', 'SEP IRA', 'Spousal IRA', 'Inherited IRA', 'Group IRA', 
       'Rollover IRA'],
       iraModel: [],
+      listOfCds: ['Traditional CD', 'Bump-up CD', 'Step-up CD', 'Liquid CD', 'Zero-coupon CD', 
+      'Callable CD', 'Brokered CD', 'High-yield CD', 'Jumbo CD', 'IRA CD', 'Add-on CD', 'Foreign Currency CD'],
+      cdModel: [],
+      cdTerms: [],
     }
   },
   methods: {
+    async addCdTerms() {
+      for(var i = 0; i < 121; i++) {
+        this.cdTerms.push( i+ ' month')
+      }
+    },
     upOne() {
       this.carouselModel += 1;
     },
@@ -330,10 +420,34 @@ export default {
       this.$store.commit('fpTool/clientSavings', this.savings);
       this.carouselModel += 1
     },
+    updateIraModel(val) {
+      if(!this.iraModel.includes(val)) {
+        this.iraModel.push(val);
+      } else {
+        const index = this.iraModel.indexOf(val)
+        this.iraModel.splice(index, 1);
+      }
+    },
     updateIraList() {
-      if(this.iraList.length > 0) { 
-        this.iraList.forEach(item => {
+      if(this.iraModel.length > 0) { 
+        this.iraModel.forEach(item => {
           this.iras.push({type: item, value: 0})
+        })
+      };
+      this.carouselModel += 1
+    },
+    updateCdModel(val) {
+      if(!this.cdModel.includes(val)) {
+        this.cdModel.push(val);
+      } else {
+        const index = this.cdModel.indexOf(val)
+        this.cdModel.splice(index, 1);
+      }
+    },
+    updateCdList() {
+      if(this.cdModel.length > 0) { 
+        this.cdModel.forEach(item => {
+          this.cds.push({type: item, deposit_amount: 0, term: 0, deposit_date: 0, apy: 0})
         })
       };
       this.carouselModel += 1
@@ -394,6 +508,9 @@ export default {
       this.$store.commit('fpTool/clientOtherAssets', this.otherAssets);
       this.carouselModel += 1
     },
+  },
+  mounted() {
+    this.addCdTerms()
   }
 }
 </script>
